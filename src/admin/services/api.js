dac605f289d97ce2069362a849896e441b1516cd
kishanyadav0900 <kishanyadav0900@gmail.com>
2026-05-08 22:40:29 +0530

    fix: revert to hardcoded supabase keys for netlify deployment

--- src/admin/services/api.js
+++ src/admin/services/api.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://rfpqwypgpminypfukarq.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmcHF3eXBncG1pbnlwZnVrYXJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NzUwOTIsImV4cCI6MjA5MjQ1MTA5Mn0.7gncRErAR362iG1WZBUK1R0lQ8waaJbRgFkPWxJ6cNQ";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
